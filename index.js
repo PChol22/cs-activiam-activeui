46421ad5a265f056c4de841c6c74496e4c1da1d9
Pchol22 <pierre.chollet@student-cs.fr>
2021-11-04 15:32:54 +0100

    1.1.1 -> minor repo changes

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const helloWorld = () => {
-    console.log('Hello World !');
-};
-
-const sayHello = (name) => {
-    return `Hello ${name} !`;
-}
+const helloWorld = require('./src/hello-world');
+const sayHello = require('./src/say-hello');
 
 module.exports = {
     helloWorld,
