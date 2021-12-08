2e9f0887b2a12c8d3ec0cd1477e86d556decf196
Barka Mohamed Ali <mohamedali.barka@student-cs.fr>
2021-12-09 00:37:33 +0100

    tic-tac-toe component

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const helloWorld = require('./src/hello-world');
 const sayHello = require('./src/say-hello');
+var ticTacToe = require("./src/tic-tac-toe/src/index")
 
 module.exports = {
     helloWorld,
     sayHello,
+    ticTacToe
 };
