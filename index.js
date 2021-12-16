058e4a214b84cb111f125e044def2c618eb69a2a
Pchol22 <pierre.chollet@student-cs.fr>
2021-12-16 13:25:23 +0100

    prettier rewrite

--- index.js
+++
@@ -1,9 +0,0 @@
-const helloWorld = require('./src/hello-world');
-const sayHello = require('./src/say-hello');
-var ticTacToe = require("./src/tic-tac-toe/src/index")
-
-module.exports = {
-    helloWorld,
-    sayHello,
-    ticTacToe
-};
